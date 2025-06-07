d0e1dceab23059c4d1d5c8bad663558322791cbe
davidsmithrojas <daviid.s.r@hotmail.com>
2025-06-06 23:41:38 -0400

    Add files via upload

---
+++ Proyecto 3/Proyecto_paginaWeb/js/app.js
@@ -0,0 +1,39 @@
+let btnMenu = document.querySelector('.btn-menu')
+let menu = document.querySelector('.list-container');
+let containerMenu = document.querySelector('.menu');
+let activador=true;
+
+btnMenu.addEventListener('click' , () => {
+
+    document.querySelector('.btn-menu i').classList.toggle('fa-times');
+
+    if(activador){
+    menu.style.left = "0";
+    menu.style.transition = "0.5s";
+
+    activador = false;
+
+    }else{
+        activador = false;
+    
+    menu.style.left = "-100%";
+    menu.style.transition = "0.5s";
+    
+    activador = true;
+    }
+});
+
+let enlaces = document.querySelectorAll('.list li a');
+
+enlaces.forEach( (element) => {
+
+    element.addEventListener('click',(event) => {
+
+        anlaces.forEach( (link) => {
+            link.classList.remove('activo');
+        });
+    });
+
+});
+
+
